a6a83fa16f2f4f093f3ff5185f61ff7c69539bf0
Vinh Thanh Vu <vtv@vinh.com>
2015-09-15 00:21:54 -0400

    Example 1: Bool, Int Float Double

--- Lesson1_Types&Operators.playground/Contents.swift
+++ Lesson1_Types&Operators.playground/Contents.swift
@@ -3,10 +3,17 @@
 import UIKit
 import Foundation
 
+// Example 1: Bool, Int Float Double
+
+
 var str = "Hello, playground"
 
 class LightSwith {
     var on: Bool = true
+//  var dimmer: Int = 7
+//  var dimmer: Float = 3.14
+    var dimmer: Double = 3.14159265359
+    
 }
 
 var livingRoomSwitch = LightSwith()
