36da6809f21adfd3f1bf38b38b6fe06aec3f1e22
Vinh Thanh Vu <vtv@vinh.com>
2015-09-14 23:51:42 -0400

    import Foundation
    Bool, Int, Float, Double

--- Lesson1_Types&Operators.playground/Contents.swift
+++ Lesson1_Types&Operators.playground/Contents.swift
@@ -1,5 +1,13 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import Foundation
 
 var str = "Hello, playground"
+
+class LightSwith {
+    var on: Bool = true
+}
+
+var livingRoomSwitch = LightSwith()
+livingRoomSwitch.on
