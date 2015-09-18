58b7a2049f9970d98c7137403fe82858e139bad8
Vinh Thanh Vu <vtv@vinh.com>
2015-09-18 00:26:44 -0400

    String with variable and expression

--- Lesson1_Strings.playground/Contents.swift
+++ Lesson1_Strings.playground/Contents.swift
@@ -23,3 +23,15 @@ var doggyDiet = "Lulu eats 25lbs of dog food per month"
 var dogName = "Ferris"
 //var ferrisPic = UIImage(named:"SpringerdoodleFerris.jpg")!
 doggyDiet = "\(dogName) eats 25lbs of dog food per month"
+
+
+// String with variables and expression
+var lbsPerDay = 0.75
+var daysPerMonth:Double = 30.0
+doggyDiet = "\(dogName) eats \(lbsPerDay * daysPerMonth) of dog food per month"
+
+
+var frankiePic = UIImage(named: "frankie.jpg")
+lbsPerDay = 0.25
+dogName = "Lil Frankie"
+doggyDiet = "\(dogName) eats \(lbsPerDay * daysPerMonth)lbs of dog food per moth"
