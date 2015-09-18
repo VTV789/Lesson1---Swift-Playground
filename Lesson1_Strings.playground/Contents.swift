f65e6a78b77916bd3e5532a68d5f0056b8d40091
Vinh Thanh Vu <vtv@vinh.com>
2015-09-17 23:42:50 -0400

    // Defining Strings using string literals

--- Lesson1_Strings.playground/Contents.swift
+++ Lesson1_Strings.playground/Contents.swift
@@ -1,5 +1,15 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import Foundation
 
-var str = "Hello, playground"
+//var funWithStrings = UIImage(named: "kittenWithOrangeString.jpg")!
+
+// Defining Strings using string literals
+let myFirstString = "mo $"
+let mySecondString = "mo problems"
+
+// String concatenation 
+let theTruth = myFirstString + ", " + mySecondString
+
+
