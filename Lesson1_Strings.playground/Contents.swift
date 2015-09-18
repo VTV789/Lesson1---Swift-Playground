508b0fcf653a597f4abc4f0e30fa17c06eaafe6e
Vinh Thanh Vu <vtv@vinh.com>
2015-09-18 00:07:01 -0400

    String with variables

--- Lesson1_Strings.playground/Contents.swift
+++ Lesson1_Strings.playground/Contents.swift
@@ -12,4 +12,14 @@ let mySecondString = "mo problems"
 // String concatenation 
 let theTruth = myFirstString + ", " + mySecondString
 
+// Emoji characters
+let similarTruth = "💰 can't buy me 💖"
 
+// Plan string
+var doggyDiet = "Lulu eats 25lbs of dog food per month"
+
+// String with variables 
+
+var dogName = "Ferris"
+//var ferrisPic = UIImage(named:"SpringerdoodleFerris.jpg")!
+doggyDiet = "\(dogName) eats 25lbs of dog food per month"
