feec0fd6c4f8d03f73ed5f07420cc07b154fe5dc
Vinh Thanh Vu <vtv@vinh.com>
2015-09-21 23:59:04 -0400

    Example 1,2ab

--- Lesson1_Constants&Variables.playground/Contents.swift
+++ Lesson1_Constants&Variables.playground/Contents.swift
@@ -12,5 +12,14 @@ let encouragement = "You can do it!"
 var personalizedEncouragement = " YOu can do it, Lauren!", withString: "cameron")
 personalizedEncouragment= personalizedEncouragement.stringByReplacingOccurrencesOfString("Lauren", withString: "Cameron")
 
+// Example 2a
+let birthday = 2008
+let currentYear = 2015
+let age = currentYear - birthYear
 
+// Example 2b - What if age only needs to be calculated once?
 
+//Example 3
+let birthweight = "6 lbs 7 ounces"
+var currentweight = "221lbs"
+var chubbyBaby = UIImageView(named:fat-baby-picture.jpg")!
