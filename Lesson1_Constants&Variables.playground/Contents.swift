4ad4336b96e798900bea14cd6c59f8e18fb53251
Vinh Thanh Vu <vtv@vinh.com>
2015-09-22 01:11:50 -0400

    Two ways of being immutable - #1 Assignment

--- Lesson1_Constants&Variables.playground/Contents.swift
+++ Lesson1_Constants&Variables.playground/Contents.swift
@@ -1,37 +1,28 @@
-//: Constants and Variables
+//: # Constants and Variables
 
 import UIKit
 
-// Example 1
-// Use let when you know the value will say constant
-
+//: ### Example 1
+//: ### Use let when you know the value will stay constant
 let encouragement = "You can do it!"
+//: ### Use var when you expect the value to change
+var personalizedEncouragement = "You can do it, Lauren!"
+personalizedEncouragement = personalizedEncouragement.stringByReplacingOccurrencesOfString("Lauren", withString: "Cameron")
+//: ### Example 2a
+let birthYear = 2008
+var currentYear = 2015
+var age = currentYear - birthYear
+//: ### Example 2b - What if age only needs to be calculated once?
 
-// Use var when you expect the value to change
-
-var personalizedEncouragement = " YOu can do it, Lauren!", withString: "cameron")
-personalizedEncouragment= personalizedEncouragement.stringByReplacingOccurrencesOfString("Lauren", withString: "Cameron")
-
-// Example 2a
-let birthday = 2008
-let currentYear = 2015
-let age = currentYear - birthYear
-
-// Example 2b - What if age only needs to be calculated once?
-
-//Example 3
+//: ### Example 3
 let birthweight = "6 lbs 7 ounces"
-var currentweight = "221lbs"
-var chubbyBaby = UIImageView(named:fat-baby-picture.jpg")!
+var currentWeight = "22lbs"
+var chubbyBaby = UIImage(named:"chubby-baby-picture.jpg")!
 
-// Let vs. Var Quiz
-let buildingCapcity = 300
-var attendance = 220
-attendance += 2
-
-// Two ways of being immutable - #1 Assignment 
+//: ### Two ways of being immutable - #1 Assignment
 var goat = UIImage(named:"Chinese-New-Year-3.jpg")!
 let yearsOfTheGoat = [1967, 1979, 1991, 2003, 2015]
 let yearsOfTheSheep = [1967, 1979, 1991, 2003, 2015]
-yearsOfTheGoat = yearsOfTheSheep
-
+//yearsOfTheGoat = yearsOfTheSheep
+//: ### Two ways of being immutable - #2 Value alteration
+//yearsOfTheGoat.append(2027)
