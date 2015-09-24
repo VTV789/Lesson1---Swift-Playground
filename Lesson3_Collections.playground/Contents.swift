c6179fe6e4174bccece211059107c008816854bd
Vinh Thanh Vu <vtv@vinh.com>
2015-09-23 23:36:59 -0400

    Array Literal sytax

--- Lesson3_Collections.playground/Contents.swift
+++ Lesson3_Collections.playground/Contents.swift
@@ -16,3 +16,6 @@ var numbers = Array<Double>()
 // More often you will see ...
 var moreNumbers = [Double]()
 moreNumbers = [85.0, 90.0, 95.0]
+
+// Array Literal sytax
+let differentNumbers = [97.5, 98.5, 99.0]
