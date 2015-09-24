0364562711900c032a366f4cfbd5fab1613d5a97
Vinh Thanh Vu <vtv@vinh.com>
2015-09-23 23:34:18 -0400

    The verbose way
    Initializing Arrays

--- Lesson3_Collections.playground/Contents.swift
+++ Lesson3_Collections.playground/Contents.swift
@@ -1,5 +1,15 @@
 //: Lesson 3 Exercises - Collections
 
 import UIKit
+import Foundation
 
-var str = "Hello, playground"
+// Array - ordered list of items
+
+// Dictionary - collection of key-value pairs
+
+// Set - unordered list of distinct values
+
+// Initializing Arrays
+// The verbose way
+var numbers = Array<Double>()
+
